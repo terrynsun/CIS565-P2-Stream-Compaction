64c5cdfbe23f11716d3481d71ca43616870a7b5a
Terry Sun <terrynsun@gmail.com>
2015-09-13 19:28:16 -0400

    Use device_vector to remove mem access from exclusive_scan.

--- stream_compaction/thrust.cu
+++ stream_compaction/thrust.cu
@@ -13,7 +13,10 @@ namespace Thrust {
  * Performs prefix-sum (aka scan) on idata, storing the result into odata.
  */
 void scan(int n, int *odata, const int *idata) {
-    thrust::exclusive_scan(idata, idata + n, odata);
+    thrust::device_vector<int> ivec(idata, idata+n);
+    thrust::device_vector<int> ovec(odata, odata+n);
+    thrust::exclusive_scan(ivec.begin(), ivec.end(), ovec.begin());
+    thrust::copy(ovec.begin(), ovec.end(), odata);
 }
 
 }
